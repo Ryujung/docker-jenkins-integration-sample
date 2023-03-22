c1461e598857bcf9e1196f702cf3f6aed4861507
ryujung <ryujung@163.com>
2023-03-23 02:59:02 +0800

    third time commit.Still wirking on jenkins build function.

--- src/main/java/com/ryujung/intergration/Test.java
+++ src/main/java/com/ryujung/intergration/Test.java
@@ -4,5 +4,6 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("test jenkins build");
+        System.out.println("test jenkins build second time");
     }
 }
